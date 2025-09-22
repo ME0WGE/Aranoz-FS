da09a3b64981e842ffe925fe1a7d6192b01d5f8a
Kamil Baldyga <baldygakamil@gmail.com>
2025-09-22 09:30:57 +0200

    add products migration

---
+++ database/migrations/2025_09_22_072121_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->boolean('pinned');
+            $table->string('image_1');
+            $table->string('image_2');
+            $table->string('image_3');
+            $table->string('image_4');
+            $table->foreignId('product_category_id')->constrained('product_categories');
+            $table->foreignId('discount_id')->constrained('discounts');
+            $table->foreignId('color_id')->constrained('colors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
