c620b9e07c9483a28f09255edcf9ba6b6a5b6d36
Kamil Baldyga <baldygakamil@gmail.com>
2025-09-22 10:04:16 +0200

    add billing model

--- app/Models/Billing.php
+++ app/Models/Billing.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Billing extends Model
 {
-    //
+    protected $fillable = [
+        'first_name', 
+        'last_name', 
+        'company',
+        'phone_number',
+        'address',
+        'number',
+        'city',
+        'zip_code',
+        'user_id',
+        'country_id'
+    ];
 }
