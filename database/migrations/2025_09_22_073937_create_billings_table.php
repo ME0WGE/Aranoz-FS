7d2c9c84470376e73df477b2e3ab7c67f4f265a5
Kamil Baldyga <baldygakamil@gmail.com>
2025-09-22 09:41:30 +0200

    add billings migrations

---
+++ database/migrations/2025_09_22_073937_create_billings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('company')->nullable();
+            $table->string('phone_number');
+            $table->string('address');
+            $table->integer('number');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('country_id')->constrained('countries');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('billings');
+    }
+};
