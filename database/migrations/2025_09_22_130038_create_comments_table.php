fc678782ca07d56d5dc0662ec8b070c1247a07f9
Kamil Baldyga <baldygakamil@gmail.com>
2025-09-22 15:02:15 +0200

    add comments table

---
+++ database/migrations/2025_09_22_130038_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('message');
+            $table->string('email');
+            $table->string('website');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('blog_id')->constrained('blogs');
+            $table->foreignId('product_id')->constrained('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
