5503ba52bd6ae7f9401e47e4b1491c9e5a2078ee
Kamil Baldyga <baldygakamil@gmail.com>
2025-09-23 16:26:41 +0200

    add navbar component

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,25 +1,11 @@
+import Nav from '@/Components/Nav';
 import { Head, Link } from '@inertiajs/react';
 
 export default function Home({ auth }) {
     return (
         <>
             <Head title="home" />
-            <nav className="flex flex-1 justify-end">
-                {auth.user ? (
-                    <Link href={route('dashboard')} className="rounded-md px-3 py-2 text-black">
-                        Dashboard
-                    </Link>
-                ) : (
-                    <>
-                        <Link href={route('login')} className="rounded-md px-3 py-2 text-black">
-                            Log in
-                        </Link>
-                        <Link href={route('register')} className="rounded-md px-3 py-2 text-black">
-                            Register
-                        </Link>
-                    </>
-                )}
-            </nav>
+            <Nav auth={auth}/>
         </>
     );
 }
